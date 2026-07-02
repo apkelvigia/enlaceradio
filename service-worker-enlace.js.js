82efbaa16bae7a1e328066c3e9b3d89fac573e90
apk-elvigia <evadenis796@gmail.com>
2026-07-02 09:51:14 -0300

    Add files via upload

---
+++ service-worker-enlace.js.js
@@ -0,0 +1,35 @@
+﻿const CACHE_NAME = 'enlace-consecuente-v1';
+const ARCHIVOS_CACHE = [
+  './',
+  './index.html',
+  './manifest-enlace.json',
+  'https://i.imgur.com/Bp36iz0.jpeg'
+];
+
+// Instalación: guarda archivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ARCHIVOS_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activación: limpia versiones viejas
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Sirve contenido desde caché o red
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(respuesta => respuesta || fetch(event.request))
+  );
+});

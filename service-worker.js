923ca5ce477ce317c6db49c3b371ba6f731c494f
apk-elvigia <evadenis796@gmail.com>
2026-07-02 12:47:43 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+﻿self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Permite que la transmisión de radio pase libremente sin trabarse en caché
+});
